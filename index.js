28f9f476eff5ec670c5b147c3185b1c55cd6595d
Malick Roungou <mjbroungou@gmail.com>
2024-11-07 07:54:38 -0500

    updates

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-document.querySelectorAll('.faq').forEach(faq => 
-    faq.addEventListener('click', () => faq.classList.toggle('active'))
-);
+/* opens and closes the faq accordions */
+document.querySelectorAll('.accordion').forEach(faq => {
+    faq.addEventListener('click', () =>  {
+        faq.classList.toggle('active')
+    const panel = faq.nextElementSibling
+
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null
+    } else {panel.style.maxHeight = panel.scrollHeight + "rem"}
+    })
+}); 
